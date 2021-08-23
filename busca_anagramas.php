a9ad449428e36e64ed2195faf359e94895c6c60a
Daniel Gil Jara <danielgiljara@gmail.com>
2021-08-23 20:06:23 +0200

    Added different features to list existent anagramas (added the index also). Added also a feature to clean the dictionary.

--- busca_anagramas.php
+++ busca_anagramas.php
@@ -1,33 +1,115 @@
 <?php
 
-$input = $argv[1];
+function get_palabras() {
+  $palabras = file_get_contents('diccionarios/diccionario.txt');
+  $palabras = explode("\n", $palabras);
+  return $palabras;
+}
 
-$palabras = file_get_contents('diccionarios/diccionario.txt');
-$palabras = explode("\n", $palabras);
+function get_anagramas($input) {
+  $palabras = get_palabras();
 
-foreach ($palabras as $palabra) {
-  if ($palabra === $input) {
-    continue;
+  if (!in_array($input, $palabras)) {
+    return FALSE;
   }
-  if (strlen($input) != strlen($palabra)) {
-    continue;
-  }
-  $palabra_array = str_split($palabra);
-  $input_array = str_split($input);
 
-  foreach ($palabra_array as $j => $palabra_letter) {
-    $found = false;
-    foreach($input_array as $k => $input_letter) {
-      if ($palabra_letter === $input_letter) {
-        $found = true;
-        unset($input_array[$k]);
+  $result = [];
+  foreach ($palabras as $palabra) {
+    if ($palabra === $input) {
+      continue;
+    }
+    if (strlen($input) != strlen($palabra)) {
+      continue;
+    }
+    $palabra_array = str_split($palabra);
+    $input_array = str_split($input);
+
+    foreach ($palabra_array as $j => $palabra_letter) {
+      $found = false;
+      foreach($input_array as $k => $input_letter) {
+        if ($palabra_letter === $input_letter) {
+          $found = true;
+          unset($input_array[$k]);
+        }
+      }
+      if (!$found) {
+        continue(2);
       }
     }
-    if (!$found) {
-      continue(2);
+    if (!$input_array) {
+      $result[] = $palabra;
     }
   }
-  if (!$input_array) {
-    echo "He encontrado el siguiente anagrama: " . $palabra . "\n";
-  }
+  return $result;
 }
+
+function get_anagramas_list() {
+  $index = unserialize(file_get_contents('index.serialized'));
+  return $index;
+}
+
+function build_index() {
+  echo "Building index...\n";
+  $palabras = get_palabras();
+  $result = [];
+  foreach ($palabras as $palabra) {
+    echo $palabra . "\n";
+    $anagramas = get_anagramas($palabra);
+    if ($anagramas) {
+      $result[$palabra] = $anagramas;
+    }
+  }
+  echo "Writing index...\n";
+  file_put_contents('index.serialized', serialize($result));
+  return $result;
+}
+
+function clear_dictionary() {
+  $palabras = file_get_contents('diccionarios/diccionario.txt');
+  $palabras = explode("\n", $palabras);
+  foreach ($palabras as $k => $v) {
+    if (is_numeric($v)) {
+      unset($palabras[$k]);
+    }
+  }
+  $palabras = implode("\n", $palabras);
+  file_put_contents('diccionarios/diccionario.txt', $palabras);
+}
+
+$op = $argv[1];
+
+switch($op) {
+  case 'get':
+    $input = $argv[2];
+    $anagramas = get_anagramas($input);
+    if ($anagramas === FALSE) {
+      echo "$input no es una palabra válida\n";
+      break;
+    }
+    foreach ($anagramas as $anagrama) {
+      echo "He encontrado el siguiente anagrama: " . $anagrama . "\n";
+    }
+    break;
+  case 'list':
+    $anagramas_list = get_anagramas_list();
+    foreach ($anagramas_list as $palabra => $anagramas) {
+      if (!$anagramas) {
+        continue;
+      }
+      echo $palabra . " (" . count($anagramas) . "):\n";
+      foreach ($anagramas as $anagrama) {
+        echo "- " . $anagrama . "\n";      
+      }
+    }
+    break;
+  case 'build-index':
+    build_index();
+    break;
+  case 'clear-dictionary':
+    clear_dictionary();
+    break;
+
+  default:
+    echo "Unkown operation.\n";
+}
+
